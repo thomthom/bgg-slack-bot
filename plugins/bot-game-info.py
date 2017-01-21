4dfbc0505a9978ab6b05e6663ec02277e6dbd385
Stuart Boston <omertron@gmail.com>
2017-01-21 14:52:24 +0000

    updated game search and help

--- plugins/bot-game-info.py
+++ plugins/bot-game-info.py
@@ -3,6 +3,8 @@ from boardgamegeek import BoardGameGeek
 
 # Import regex lib
 import re
+# Import json lib
+import json
 
 bgg = BoardGameGeek()
 
@@ -25,17 +27,79 @@ def game_search(message, searchtext):
     else:
         game_message = "\nNo games found for that search"
 
-    message.send_webapi(game_message, None, True)
+    message.send_webapi(game_message, '', True)
 
 def format_search_message(game):
     if game is None:
         return
 
-    msg = "\n%s ( <https://boardgamegeek.com/boardgame/%s|%s> )" % (
+    msg = "\n%s (<https://boardgamegeek.com/boardgame/%s|%s>)" % (
         game.name,
         game.id,
         game.id
         )
 
     return msg
+
+@respond_to('game (.*)')
+@listen_to('\[\[game (.*)\]\]')
+def game_search(message, gametext):
+	print('got game request for %s' % gametext)
+
+	if unicode(gametext, 'utf-8').isnumeric():
+		message.reply('Getting game info for ID *{}*'.format(gametext))
+		game = bgg.game(None, gametext)
+	else:
+                message.reply('Getting game info for *{}*'.format(gametext))
+                game = bgg.game(gametext, None)
+
+        if game == None:
+                message.reply('No information found for *{}*'.format(gametext))
+                return
+
+    	attachments = [
+    	{
+        	'fallback': 'Information on ' + game.name,
+        	'author_name': game.name + ' (' + str(game.yearpublished) + ')',
+        	'author_link': 'https://boardgamegeek.com/boardgame/' + str(game.id),
+		'author_icon': game.thumbnail,
+        	'text': game.description,
+        	'color': 'good',
+#           	'image_url': game.image,
+        	'thumb_url': game.thumbnail,
+		'fields' : [
+                {
+                        'title' : 'BGG ID',
+                        'value' : game.id,
+                        'short' : True
+                },
+		{
+			'title' : 'Player Count',
+			'value' : str(game.minplayers) + '-' + str(game.maxplayers),
+			'short' : True
+		},
+		{
+                        'title' : 'Playing Time',
+                        'value' : game.playingtime,
+                        'short' : True
+
+		},
+		{
+                        'title' : 'Designer(s)',
+                        'value' : ', '.join(game.designers),
+                        'short' : True
+		},
+		{
+                        'title' : 'Categories',
+                        'value' : ', '.join(game.categories),
+                        'short' : True
+		},
+                {
+                        'title' : 'Mechanics',
+                        'value' : ', '.join(game.mechanics),
+                        'short' : True
+                }]
+
+    	}]
+    	message.send_webapi('', json.dumps(attachments))
 
--- plugins/bot-help.py
+++ plugins/bot-help.py
@@ -2,14 +2,18 @@ from slackbot.bot import respond_to,listen_to,default_reply
 from boardgamegeek import BoardGameGeek
 
 import re
+import sys
 
 bgg = BoardGameGeek()
 
 @respond_to('help', re.IGNORECASE)
-@listen_to('help bgg', re.IGNORECASE)
+@listen_to('[[help]]', re.IGNORECASE)
 def help(message):
         print('got command help')
-	help_message = "The following commands are available from the game bot: \n\n" +\
-		"\t\tgame <game title> -- Search for information about a game. \n"
+	help_message = "The following commands are available from the game bot:\n\n" +\
+		"\t*search* `text`\t\t\tSearch for information on `text`.\n" +\
+		"\t*game* `game title`\tGet information about `game title`. Must be the exact name of the game.\n" +\
+		"\t*game* `BGG ID`\t\t\tGet information about the game with this `BGG ID`.\n\n" +\
+		""
 	message.send(help_message)
 
