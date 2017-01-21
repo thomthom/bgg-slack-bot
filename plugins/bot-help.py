b4b68784278661b78e0ccfb15936c2ec8f8405c6
Stuart Boston <omertron@gmail.com>
2017-01-21 21:33:23 +0000

    Updated bot files

--- plugins/bot-help.py
+++ plugins/bot-help.py
@@ -7,13 +7,15 @@ import sys
 bgg = BoardGameGeek()
 
 @respond_to('help', re.IGNORECASE)
-@listen_to('[[help]]', re.IGNORECASE)
+@listen_to('\[\[help\]\]', re.IGNORECASE)
 def help(message):
         print('got command help')
 	help_message = "The following commands are available from the game bot:\n\n" +\
-		"\t*search* `text`\t\t\tSearch for information on `text`.\n" +\
-		"\t*game* `game title`\tGet information about `game title`. Must be the exact name of the game.\n" +\
-		"\t*game* `BGG ID`\t\t\tGet information about the game with this `BGG ID`.\n\n" +\
+		"*search* `game name`\n\t\tSearch for games called _game name_. Does not need to be exact.\n" +\
+		"*game* `game title`\n\tGet information about _game title_. Must be the exact name of the game.\n" +\
+		"*game* `BGG ID`\n\tGet information about the game with this _BGG ID_.\n\n" +\
+		"*user* `BGG username`\n\tGet information on a BGG user.\n" +\
+		"*coll* `BGG username`\n\tGet a list of the owned games for a user." +\
 		""
 	message.send(help_message)
 
--- plugins/bot-user-info.py
+++ plugins/bot-user-info.py
@@ -1,25 +1,82 @@
-from slackbot.bot import respond_to,listen_to,default_reply
+from slackbot.bot import respond_to,listen_to
 from boardgamegeek import BoardGameGeek
 
-import re
+import re, json
 
 bgg = BoardGameGeek()
 
-#@default_reply
-#def my_default_hanlder(messsage):
-#    print('got default handler')
-#    message.reply('Im sorry, this bot does not understand you, please try the *help* command')
+def jointwo(string1, string2, joinstr):
+        return joinstr.join([x for x in (string2, string2) if x])
 
-@respond_to('game (.*)')
-@listen_to('\[\[game (.*)\]\]')
-def game(message, something):
-    print('got game request')
-    message.reply('Getting information on *{}*'.format(something))
-    bgg = BoardGameGeek()
-    g = bgg.game(something)
+@respond_to('user (.*)')
+@listen_to('\[\[user (.*)\]\]')
+def user_info(message, username):
+	print('Getting information on user ' + username)
+	message.reply('Getting user information on *{}*'.format(username))
 
-    game_message = "\nTitle: " + g.name +\
-	"\nID: " + g.id +\
-	"\nDescription: " + g.description
-    message.send(game_message)
+	user = bgg.user(username)
+
+	if user == None:
+		message.reply('No information found for user *{}*'.format(username))
+		return
+
+    	attachments = [{
+        	'fallback' : 'User information on ' + username,
+                'author_name': jointwo(user.firstname, user.lastname, ' ') + ' (' + user.name + ')',
+                'author_link': 'https://boardgamegeek.com/user/' + user.name,
+                'author_icon': 'https:' + user.avatarlink,
+                'color': 'good',
+                'thumb_url': 'https:' + user.avatarlink,
+                'fields' : [
+                {
+                        'title' : 'Year Registered',
+                        'value' : user.yearregistered,
+                        'short' : True
+                },
+                {
+                        'title' : 'Location',
+                        'value' : jointwo(user.stateorprovince, user.country, ' / '),
+                        'short' : True
+                }]
+
+
+	}]
+    	message.send_webapi('', json.dumps(attachments))
+
+
+@respond_to('coll (.*)')
+@listen_to('\[\[coll (.*)\]\]')
+def coll_info(message, username):
+	print('Getting collection information for user ' + username)
+        message.reply('Getting collection information for *{}*, this may take some time...'.format(username))
+
+	coll = bgg.collection(username)
+
+        if coll == None:
+		message.reply('No collection information found for user *{}*'.format(username))
+		return
+
+	count = 0
+	coll_list = ''
+	for x in coll:
+		if x.own == '1':
+			if x.rating == None:
+                                myrating = 'Not Rated'
+			else:
+                                myrating = "\tRating: " + str(x.rating)
+
+			coll_list += "\n%s (<https://boardgamegeek.com/boardgame/%s|%s>) %s" % (
+				x.name, 
+				x.id, x.id, 
+				myrating
+			)
+			count += 1
+
+	if count > 0:
+		coll_msg = "Found %s owned games for %s\n" % (count, username)
+		coll_msg += coll_list
+	else:
+		coll_msg = "No owned games in the collection for " + username
+
+	message.send_webapi(coll_msg, '', True)
 
